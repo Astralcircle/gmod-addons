de2bf8533285994713be4fa96fa35753a642ef60
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-05-21 02:45:27 +0300

    Remove most of the unnecessary NW networking + replace the remaining with NW2

--- extended_properties_104607712/lua/autorun/rb655_ext_props.lua
+++ extended_properties_104607712/lua/autorun/rb655_ext_props.lua
@@ -43,53 +43,11 @@ end
 
 if ( SERVER ) then
 
-	local SyncFuncs = {}
-
-	SyncFuncs.prop_door_rotating = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "m_bLocked" ) )
-		local state = ent:GetInternalVariable( "m_eDoorState" )
-		ent:SetNWBool( "Closed", state == 0 or state == 3 )
-	end
-	SyncFuncs.func_door = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "m_bLocked" ) )
-		--[[local state = ent:GetInternalVariable( "m_eDoorState" )
-		ent:SetNWBool( "Closed", state == 0 or state == 3 )]]
-	end
-	SyncFuncs.func_door_rotating = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "m_bLocked" ) )
-		--[[local state = ent:GetInternalVariable( "m_eDoorState" )
-		ent:SetNWBool( "Closed", state == 0 or state == 3 )]]
-	end
-	SyncFuncs.prop_vehicle_jeep = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "VehicleLocked" ) )
-		ent:SetNWBool( "HasDriver", IsValid( ent:GetDriver() ) )
-		ent:SetNWBool( "m_bRadarEnabled", ent:GetInternalVariable( "m_bRadarEnabled" ) )
-	end
-	SyncFuncs.prop_vehicle_airboat = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "VehicleLocked" ) )
-		ent:SetNWBool( "HasDriver", IsValid( ent:GetDriver() ) )
-	end
-	--[[SyncFuncs.prop_vehicle_prisoner_pod = function( ent )
-		ent:SetNWBool( "Locked", ent:GetInternalVariable( "VehicleLocked" ) )
-		ent:SetNWBool( "HasDriver", IsValid( ent:GetDriver() ) )
-	end]]
-	SyncFuncs.func_tracktrain = function( ent )
-		ent:SetNWInt( "m_dir", ent:GetInternalVariable( "m_dir" ) )
-		ent:SetNWBool( "m_moving", ent:GetInternalVariable( "speed" ) != 0 )
-		--[[local driver = ent:GetDriver()
-		ent:SetNWBool( "HasDriver", IsValid( driver ) )]]
-	end
-
-	-- Periodically sync server data to clients.
-	local nextSync = 0
-	hook.Add( "Tick", "rb655_propperties_sync", function()
-		if ( nextSync > CurTime() ) then return end
-		nextSync = CurTime() + 1
-
-		for id, ent in ents.Iterator() do
-			if ( SyncFuncs[ ent:GetClass() ] ) then
-				SyncFuncs[ ent:GetClass() ]( ent )
-			end
+	-- Periodically sync data func train with the client
+	timer.Create( "rb655_propperties_sync_tracktrain", 1, 0, function()
+		for id, ent in ipairs(ents.FindByClass("func_tracktrain")) do
+			ent:SetNW2Int( "m_dir", ent:GetInternalVariable( "m_dir" ) )
+			ent:SetNW2Bool( "m_moving", ent:GetInternalVariable( "speed" ) != 0 )
 		end
 	end )
 
@@ -105,22 +63,24 @@ local ToggleIcon = "icon16/arrow_switch.png"
 -------------------------------------------------- Half - Life 2 Specific --------------------------------------------------
 
 AddEntFireProperty( "rb655_door_open", "Open", 655, function( ent, ply )
-	if ( !ent:GetNWBool( "Closed" ) and ent:GetClass() == "prop_door_rotating" ) then return false end
+	local closed = ent:GetInternalVariable( "m_eDoorState" )
+	if ( closed ~= 0 and class ~= 3 and ent:GetClass() == "prop_door_rotating" ) then return false end
 
 	return rb655_property_filter( { "prop_door_rotating", "func_door_rotating", "func_door" }, ent, ply )
 end, "Open", "icon16/door_open.png" )
 AddEntFireProperty( "rb655_door_close", "Close", 656, function( ent, ply )
-	if ( ent:GetNWBool( "Closed" ) and ent:GetClass() == "prop_door_rotating" ) then return false end
+	local closed = ent:GetInternalVariable( "m_eDoorState" )
+	if ( closed == 0 or closed == 3 and ent:GetClass() == "prop_door_rotating" ) then return false end
 
 	return rb655_property_filter( { "prop_door_rotating", "func_door_rotating", "func_door" }, ent, ply )
 end, "Close", "icon16/door.png" )
 AddEntFireProperty( "rb655_door_lock", "Lock", 657, function( ent, ply )
-	if ( ent:GetNWBool( "Locked" ) and ent:GetClass() != "prop_vehicle_prisoner_pod" ) then return false end
+	if ( ent:GetInternalVariable( ent:IsVehicle() and "VehicleLocked" or "m_bLocked" ) and ent:GetClass() != "prop_vehicle_prisoner_pod" ) then return false end
 
 	return rb655_property_filter( { "prop_door_rotating", "func_door_rotating", "func_door", "prop_vehicle_jeep", "prop_vehicle_airboat", "prop_vehicle_prisoner_pod" }, ent, ply )
 end, "Lock", "icon16/lock.png" )
 AddEntFireProperty( "rb655_door_unlock", "Unlock", 658, function( ent, ply )
-	if ( !ent:GetNWBool( "Locked" ) and ent:GetClass() != "prop_vehicle_prisoner_pod" ) then return false end
+	if ( !ent:GetInternalVariable( ent:IsVehicle() and "VehicleLocked" or "m_bLocked" ) and ent:GetClass() != "prop_vehicle_prisoner_pod" ) then return false end
 
 	return rb655_property_filter( { "prop_door_rotating", "func_door_rotating", "func_door", "prop_vehicle_jeep", "prop_vehicle_airboat", "prop_vehicle_prisoner_pod" }, ent, ply )
 end, "Unlock", "icon16/lock_open.png" )
@@ -129,23 +89,23 @@ AddEntFireProperty( "rb655_func_movelinear_open", "Start", 655, "func_movelinear
 AddEntFireProperty( "rb655_func_movelinear_close", "Return", 656, "func_movelinear", "Close", "icon16/arrow_left.png" )
 
 AddEntFireProperty( "rb655_func_tracktrain_StartForward", "Start Forward", 655, function( ent, ply )
-	if ( ent:GetNWInt( "m_dir" ) == 1 ) then return false end
+	if ( ent:GetNW2Int( "m_dir" ) == 1 ) then return false end
 
 	return rb655_property_filter( "func_tracktrain", ent, ply )
 end, "StartForward", "icon16/arrow_right.png" )
 AddEntFireProperty( "rb655_func_tracktrain_StartBackward", "Start Backward", 656, function( ent, ply )
-	if ( ent:GetNWInt( "m_dir" ) == -1 ) then return false end
+	if ( ent:GetNW2Int( "m_dir" ) == -1 ) then return false end
 
 	return rb655_property_filter( "func_tracktrain", ent, ply )
 end, "StartBackward", "icon16/arrow_left.png" )
 --AddEntFireProperty( "rb655_func_tracktrain_Reverse", "Reverse", 657, "func_tr2acktrain", "Reverse", "icon16/arrow_undo.png" ) -- Same as two above
 AddEntFireProperty( "rb655_func_tracktrain_Stop", "Stop", 658, function( ent, ply )
-	if ( !ent:GetNWBool( "m_moving" ) ) then return false end
+	if ( !ent:GetNW2Bool( "m_moving" ) ) then return false end
 
 	return rb655_property_filter( "func_tracktrain", ent, ply )
 end, "Stop", "icon16/shape_square.png" )
 AddEntFireProperty( "rb655_func_tracktrain_Resume", "Resume", 659, function( ent, ply )
-	if ( ent:GetNWInt( "m_moving" ) ) then return false end
+	if ( ent:GetNW2Int( "m_moving" ) ) then return false end
 
 	return rb655_property_filter( "func_tracktrain", ent, ply )
 end, "Resume", "icon16/resultset_next.png" )
@@ -314,7 +274,7 @@ end, "icon16/arrow_refresh.png" )
 -------------------------------------------------- Vehicles --------------------------------------------------
 
 AddEntFunctionProperty( "rb655_vehicle_exit", "Kick Driver", 655, function( ent )
-	if ( ent:IsVehicle() and ent:GetNWBool( "HasDriver" ) ) then return true end
+	if ( ent:IsVehicle() and IsValid( ent:GetDriver() ) ) then return true end
 	return false
 end, function( ent )
 	if ( !IsValid( ent:GetDriver() ) or !ent:GetDriver().ExitVehicle ) then return end
@@ -325,7 +285,7 @@ AddEntFireProperty( "rb655_vehicle_radar", "Enable Radar", 655, function( ent )
 	if ( !ent:IsVehicle() or ent:GetClass() != "prop_vehicle_jeep" ) then return false end
 	if ( ent:LookupAttachment( "controlpanel0_ll" ) == 0 ) then return false end -- These two attachments must exist!
 	if ( ent:LookupAttachment( "controlpanel0_ur" ) == 0 ) then return false end
-	if ( ent:GetNWBool( "m_bRadarEnabled", false ) ) then return false end
+	if ( ent:GetInternalVariable( "m_bRadarEnabled" ) ) then return false end
 	return true
 end, "EnableRadar", "icon16/application_add.png" )
 
@@ -333,12 +293,12 @@ AddEntFireProperty( "rb655_vehicle_radar_off", "Disable Radar", 655, function( e
 	if ( !ent:IsVehicle() or ent:GetClass() != "prop_vehicle_jeep" ) then return false end
 	-- if ( ent:LookupAttachment( "controlpanel0_ll" ) == 0 ) then return false end -- These two attachments must exist!
 	-- if ( ent:LookupAttachment( "controlpanel0_ur" ) == 0 ) then return false end
-	if ( !ent:GetNWBool( "m_bRadarEnabled", false ) ) then return false end
+	if ( !ent:GetInternalVariable( "m_bRadarEnabled" ) ) then return false end
 	return true
 end, "DisableRadar", "icon16/application_delete.png" )
 
 AddEntFunctionProperty( "rb655_vehicle_enter", "Enter Vehicle", 656, function( ent )
-	if ( ent:IsVehicle() and !ent:GetNWBool( "HasDriver" ) ) then return true end
+	if ( ent:IsVehicle() and !IsValid( ent:GetDriver() ) ) then return true end
 	return false
 end, function( ent, ply )
 	ply:ExitVehicle()
@@ -347,7 +307,7 @@ end, "icon16/car.png" )
 
 AddEntFunctionProperty( "rb655_vehicle_add_gun", "Mount Gun", 657, function( ent )
 	if ( !ent:IsVehicle() ) then return false end
-	if ( ent:GetNWBool( "EnableGun", false ) ) then return false end
+	if ( ent:GetInternalVariable( "m_bHasGun" ) ) then return false end
 	if ( ent:GetBodygroup( 1 ) == 1 ) then return false end
 	if ( ent:LookupSequence( "aim_all" ) > 0 ) then return true end
 	if ( ent:LookupSequence( "weapon_yaw" ) > 0 and ent:LookupSequence( "weapon_pitch" ) > 0 ) then return true end
@@ -357,8 +317,6 @@ end, function( ent )
 	ent:Activate()
 
 	ent:SetBodygroup( 1, 1 )
-
-	ent:SetNWBool( "EnableGun", true )
 end, "icon16/gun.png" )
 
 -------------------------------------------------- Garry's Mod Specific --------------------------------------------------
